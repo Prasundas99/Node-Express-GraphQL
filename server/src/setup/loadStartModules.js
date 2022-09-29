73aa3ddce482301b9a75ee42152e742d921fccaf
Prasundas99 <prasundas115@gmail.com>
2022-09-29 14:18:02 +0530

    graphql server running

--- server/src/setup/loadStartModules.js
+++ server/src/setup/loadStartModules.js
@@ -1,20 +1,17 @@
 import morgan from 'morgan'
 import express from 'express'
 import cors from 'cors'
-import helmet from 'helmet'
 import ExpressMongoSanitize from 'express-mongo-sanitize'
 import dotenv from 'dotenv'
 
 export default function loadStartModules(app) {
   console.info('SETUP - Loading modules...')
   dotenv.config()
-  // set security HTTP headers
-  app.use(helmet())
+
   // disable display of tech stack
   app.disable('x-powered-by')
   // parse json request body
   app.use(express.json({ limit: '69mb' }))
-  app.use(express.urlencoded({ limit: '69mb' }))
   // parse urlencoded request body
   app.use(express.urlencoded({ extended: true }))
   app.use(ExpressMongoSanitize())
